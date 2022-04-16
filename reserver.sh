5d14985eb5f471fc37c6610c4f53437f5a80ee76
Ari <ari@ceiphr.com>
2022-04-16 11:26:40 -0400

    Implemented all features I wanted.

--- reserver.sh
+++ reserver.sh
@@ -6,19 +6,20 @@
 # Copyright (c) 2022 Ari Birnbaum.
 #
 # A *very mild* contingency plan for servers that run out of space.
+# Based on this great article by Brian Schrader:
+# https://brianschrader.com/archive/why-all-my-servers-have-an-8gb-empty-file/
 
 # Default Configuration
-FILE_NAME=".reservation"         # The file that will be used to store the reservation.
-DIRECTORY=$HOME                  # The directory where the reservation is stored.
-RESERVATION_SIZE=5               # The amount of space, in gigabytes, to reserve.
-MAX_UNALLOCATED_SPACE_PERCENT=50 # The maximum percentage of unallocated space that can be reserved.
-CONFIG="$HOME/.reserver.conf"    # The configuration file.
-LOCK_FILE="/tmp/reserver.lock"   # The lock file. Prevent multiple reservations at once.
+FILE_NAME=".reservation"           # The file that will be used to store the reservation.
+DIRECTORY=$HOME                    # The directory where the reservation is stored.
+RESERVATION_SIZE=5                 # The amount of space, in gigabytes, to reserve.
+MAX_UNALLOCATED_SPACE_PERCENT=50   # The maximum percentage of unallocated space that can be reserved.
+CONFIG_FILE="$HOME/.reserver.conf" # The configuration file.
+LOCK_FILE="/tmp/reserver.lock"     # The lock file. Prevent multiple reservations at once.
+DEBUG=0                            # Set to 1 to enable debug mode.
 
 # Don't edit below this line.
-
-# TODO Ensure lock is locked/released when appropriate (e.g. in remove()).
-# TODO Add CONFIG support for flags.
+# ----------------------------------------------------------------------------------------------------------------------
 
 # For echo -e color support.
 TXT_DEFAULT='\033[0m'
@@ -28,14 +29,14 @@ TXT_RED='\033[0;31m'
 TXT_BOLD='\033[1m'
 TXT_UNBOLD_DIM='\033[0;2m'
 
+# Check the default configuration is valid.
 if [ $MAX_UNALLOCATED_SPACE_PERCENT -gt 100 ]; then
-    echo -e "${TXT_RED}MAX_UNALLOCATED_SPACE_PERCENT must be less than 100. Exiting.${TXT_DEFAULT}"
+    echo -e "${TXT_RED}MAX_UNALLOCATED_SPACE_PERCENT must be less than 100. Exiting.${TXT_DEFAULT}" >&2
     exit 1
 fi
 
-if [ ! -d "$DIRECTORY" ] || [ ! -w "$DIRECTORY" ]; then
-    echo -e "${TXT_RED}Directory is not usable or does not exist. Exiting.${TXT_DEFAULT}"
-    exit 1
+if [ $DEBUG -eq 1 ]; then
+    echo -e "Debug mode ${TXT_GREEN}enabled${TXT_DEFAULT}. Reserver will not reserve space."
 fi
 
 help() {
@@ -44,14 +45,14 @@ help() {
 Usage: $(basename "$0") [options]
 ${TXT_GREY}
 This script will reserve space on a server,
-so you can delete it later. 
+so, you can delete it later. 
 
 Specifically, when your server runs out of space, you
 can remove this reservation and the server will hopefully
 have enough space to function again while you try to fix
 whatever caused the issue.
 
-By default, will create a ${TXT_BOLD}5GB${TXT_UNBOLD_DIM} reservation file 
+By default, this script will create a ${TXT_BOLD}5GB${TXT_UNBOLD_DIM} reservation file 
 in your home directory.
 ${TXT_DEFAULT}
 Options:
@@ -69,6 +70,7 @@ EOF
 }
 
 # Tiago Lopo: https://stackoverflow.com/a/29436423/9264137
+# This is used to ask the user for confirmation.
 function yes_or_no {
     while true; do
         read -r -p "$* [y/N]: " yn
@@ -82,29 +84,75 @@ function yes_or_no {
     done
 }
 
+# Mikkel: https://unix.stackexchange.com/a/206216/378978
+# These two functions read and write to the config file.
+# This way, if the user sets a custom location and or name
+# for the reservation, we can remember it and remove
+# it when asked.
+read_config() {
+    typeset -A config
+    config=(
+        [file_name]=$FILE_NAME
+        [directory]=$DIRECTORY
+    )
+
+    while read -r line; do
+        if echo "$line" | grep -F = &>/dev/null; then
+            varname=$(echo "$line" | cut -d '=' -f 1)
+            config[$varname]=$(echo "$line" | cut -d '=' -f 2-)
+        fi
+    done <"$CONFIG_FILE"
+
+    FILE_NAME=${config[file_name]}
+    DIRECTORY=${config[directory]}
+}
+
+write_config() {
+    echo "file_name=$FILE_NAME" >"$CONFIG_FILE"
+    echo "directory=$DIRECTORY" >>"$CONFIG_FILE"
+}
+
 # Remove the reservation file if it exists.
+ret=0
 remove() {
+    # Config file remembers name and location of last reservation.
+    # Once a reservation is removed, the config file is no longer needed.
+    if [ -f "$CONFIG_FILE" ]; then
+        read_config
+        rm "$CONFIG_FILE"
+    fi
+
     if [ -f "$DIRECTORY/$FILE_NAME" ]; then
         rm "$DIRECTORY/$FILE_NAME"
         echo -e "${TXT_GREEN}Reservation removed. Good luck!${TXT_DEFAULT}"
-        exit 0
+        ret=0
     else
-        echo -e "${TXT_RED}No reservation file found.${TXT_DEFAULT}"
-        exit 1
+        echo -e "${TXT_RED}No reservation file found. Exiting.${TXT_DEFAULT}" >&2
+        ret=1
     fi
 
-    if [ -f "$LOCK_FILE" ]; then
-        rm "$LOCK_FILE"
-    fi
+    return $ret
 }
 
-if ((BASH_VERSINFO[0] < 5)); then
-    echo -e "${TXT_RED}This script was tested on GNU bash, version 5.1.8. You're using an older version. ${TXT_DEFAULT}"
+# Safety checks
+version=5
+if ((BASH_VERSINFO[0] < version)); then
+    echo -e "${TXT_RED}This script was tested on GNU bash, version ${version}."
+    echo -e "You're using an older version (v${BASH_VERSINFO[0]}).${TXT_DEFAULT}"
     message="Would you still like to continue?"
     yes_or_no "$message" || exit 1
 fi
 
-while getopts ':f:hn:d:s:rc' OPTION; do
+if [ "$EUID" -eq 0 ]; then
+    echo -e "${TXT_RED}This script isn't supposed to be run as root.${TXT_DEFAULT}"
+    message="Would you still like to continue?"
+    yes_or_no "$message" || exit 1
+    exit
+fi
+
+# Parse command line arguments.
+configured=0
+while getopts ':f:hd:s:r' OPTION; do
     case "$OPTION" in
     s)
         RESERVATION_SIZE="$OPTARG"
@@ -112,14 +160,17 @@ while getopts ':f:hn:d:s:rc' OPTION; do
         ;;
     f)
         FILE_NAME="$OPTARG"
+        configured=1
         echo "Using file: $FILE_NAME"
         ;;
     d)
         DIRECTORY="$OPTARG"
+        configured=1
         echo "Using directory: $DIRECTORY"
         ;;
     r)
         remove
+        exit $ret
         ;;
     h)
         help
@@ -138,10 +189,28 @@ done
 
 # Acquire lock.
 if [ -f "$LOCK_FILE" ]; then
-    echo -e "${TXT_RED}A reservation was already in process. Exiting.${TXT_DEFAULT}"
+    echo -e "${TXT_RED}A reservation is already in process. Exiting.${TXT_DEFAULT}" >&2
     exit 1
 else
     touch "$LOCK_FILE"
+fi
+
+# If the user configured a file name or directory, remember that,
+# so, it's not necessary to ask again when removing the reservation.
+if [ $configured -eq 1 ]; then
+    touch "$CONFIG_FILE"
+    write_config
+elif [ -f "$CONFIG_FILE" ]; then
+    read_config
+fi
+
+if [ ! -d "$DIRECTORY" ] || [ ! -w "$DIRECTORY" ]; then
+    echo -e "${TXT_RED}Directory is not usable or does not exist. Exiting.${TXT_DEFAULT}" >&2
+
+    if [ -f "$LOCK_FILE" ]; then
+        rm "$LOCK_FILE"
+    fi
+    exit 1
 fi
 
 # Reservation was already made.
@@ -152,7 +221,7 @@ if [ -f "$DIRECTORY/$FILE_NAME" ]; then
     if [ -f "$LOCK_FILE" ]; then
         rm "$LOCK_FILE"
     fi
-    exit 0
+    exit $ret
 fi
 
 echo -e "${TXT_GREEN}Reserving ${RESERVATION_SIZE}GB of space...${TXT_DEFAULT}"
@@ -160,21 +229,23 @@ echo -e "${TXT_GREEN}Reserving ${RESERVATION_SIZE}GB of space...${TXT_DEFAULT}"
 # Get the total unallocated space on the system.
 TOTAL_UNALLOCATED_SPACE=$(df -BG / | grep -v "Filesystem" | awk '{print $4}' | tail -n 1)
 if [ -z "$TOTAL_UNALLOCATED_SPACE" ]; then
-    echo -e "${TXT_RED}Unable to determine total unused disk space. Exiting.${TXT_DEFAULT}"
+    echo -e "${TXT_RED}Unable to determine total unused disk space. Exiting.${TXT_DEFAULT}" >&2
     exit 1
 fi
 
 # Determine if the reservation is too large.
 ALLOWED_UNALLOCATED_SPACE=$((${TOTAL_UNALLOCATED_SPACE::-1} * MAX_UNALLOCATED_SPACE_PERCENT / 100))
 if [ "$ALLOWED_UNALLOCATED_SPACE" -lt "$RESERVATION_SIZE" ]; then
-    echo -e "${TXT_RED}Not enough unused disk space. Exiting.${TXT_DEFAULT}"
+    echo -e "${TXT_RED}Not enough unused disk space. Exiting.${TXT_DEFAULT}" >&2
     exit 1
 fi
 
 # Create reservation file.
 CALCULATED_RESERVATION_SIZE=$((RESERVATION_SIZE * 1024))
 
-if ! command -v fallocate &>/dev/null; then
+if [ $DEBUG -eq 1 ]; then
+    touch "$DIRECTORY/$FILE_NAME"
+elif ! command -v fallocate &>/dev/null; then
     # Fallocate is not available. We're on a legacy system. Using dd instead.
     dd if=/dev/zero of="$DIRECTORY/$FILE_NAME" bs=1M count=$CALCULATED_RESERVATION_SIZE
 else
