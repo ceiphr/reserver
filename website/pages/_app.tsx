34950a5427512b5852e2dd14344348a8f6948297
Ari <ari@ceiphr.com>
2022-09-24 00:24:23 -0400

    Website formatting. Added export.

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -4,7 +4,7 @@ import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
