14f0921872952178b48cfa34a383537f25f6c3dd
Ari <ari@ceiphr.com>
2022-09-25 02:27:26 -0400

    Fixed installer content type.

--- website/next.config.js
+++ website/next.config.js
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  swcMinify: true,
-  reactStrictMode: true,
-  images: {
-    domains: ["img.shields.io"],
-  },
-  assetPrefix: isProd ? "/reserver/" : "",
-  images: {
-    unoptimized: true,
-  },
+    swcMinify: true,
+    reactStrictMode: true,
+    images: {
+        domains: ["img.shields.io"],
+    },
+    assetPrefix: isProd ? "/reserver/" : "",
+    images: {
+        unoptimized: true,
+    },
+    async headers() {
+        return [
+            {
+                source: "/install",
+                headers: [
+                    {
+                        key: "Content-Type",
+                        value: "text/html",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
